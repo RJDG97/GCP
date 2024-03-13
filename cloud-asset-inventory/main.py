367f786e3923f3e713055c5958e54448a4ef3a95
RenzoGarcia <renzogarcia@google.com>
2024-03-13 14:09:00 +0800

    Changed code to account for bq table name constraints

--- cloud-asset-inventory/main.py
+++ cloud-asset-inventory/main.py
@@ -7,21 +7,34 @@ from google.auth import default
 
 # Recursive function to define schema
 def append_data_to_schema(schema, data):
+    # New json to support the bq table names
+    new_json={}
     # Define schema dynamically based on top-level data keys and nested data structure
     for key, value in data.items():
-        if isinstance(value, list):
+        key = key.replace('.', '-')
+        key = key.replace('/', '_')
+
+        if isinstance(value, bool):
+            # Handle list (adjust data type for elements as needed)
+            schema.append(bigquery.SchemaField(key, 'BOOL'))
+            new_json[key] = value
+        elif isinstance(value, list):
             # Handle list (adjust data type for elements as needed)
             schema.append(bigquery.SchemaField(key, 'STRING', mode='REPEATED'))
+            new_json[key] = value
         elif isinstance(value, dict):
             # Handle nested data (adjust data types for nested fields)
             nested_schema = []
-            append_data_to_schema(nested_schema, value)
+            _, json = append_data_to_schema(nested_schema, value)
             schema.append(bigquery.SchemaField(key, 'RECORD', fields=nested_schema))
+            new_json[key] = json
         else:
             # Handle other data types (adjust as needed)
             schema_type = 'STRING'
             schema.append(bigquery.SchemaField(key, schema_type))
-    return schema
+            new_json[key] = value
+
+    return schema, new_json
 
 # Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
@@ -36,18 +49,19 @@ def export_cai_to_bigquery(cloud_event):
 
     message_data = json.loads(data_string)
     message_body = message_data["asset"]
-    print(f"message_body: {message_body}")
+
+    # Retrieve API and Asset name from Asset type
+    asset_api_name = message_body["assetType"].split(".googleapis.com/",1)[0]
+    asset_name = message_body["assetType"].split(".googleapis.com/",1)[1]
+    print(f"Log: {asset_api_name}.{asset_name} Processing data asset type: {asset_api_name}")
+    print(f"Log: {asset_api_name}.{asset_name} message_body: {message_body}")
 
     # Extract fields for BigQuery schema
     schema = []
-    schema = append_data_to_schema(schema, message_body)
-    print(f"schema generated: {schema}")
-    rows = tuple(message_body.values())
-    
-    # Retrieve API and Asset name from Asset type
-    asset_api_name = message_data["asset"]["assetType"].split(".googleapis.com/",1)[0]
-    asset_name = message_data["asset"]["assetType"].split(".googleapis.com/",1)[1]
-    print(f"Processing data asset type: {asset_api_name}")
+    schema, data = append_data_to_schema(schema, message_body)
+    print(f"Log: {asset_api_name}.{asset_name} schema generated: {schema}")
+    print(f"Log: {asset_api_name}.{asset_name} updated_body: {data}")
+    rows = tuple(data.values())
 
     # Seperate APIs into different datasets
     dataset_name = f"{asset_api_name}_cai_dataset"
@@ -63,11 +77,11 @@ def export_cai_to_bigquery(cloud_event):
     client.create_table(table, exists_ok=True)
 
     # Insert CAI data into BigQuery table
-    errors = client.insert_rows(table, rows)
+    errors = client.insert_rows(table, [rows])
     
     if errors:
-        print(f"Errors while inserting CAI data: {errors}")
+        print(f"ERROR: while inserting {asset_api_name}.{asset_name} CAI data: {errors}")
     else:
-        print("CAI data succesfully exported to big query")
+        print(f"SUCCESS: {asset_api_name}.{asset_name} CAI data succesfully exported to big query")
 
     return {}
